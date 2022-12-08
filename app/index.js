56a62098f85e605873b0a8505640fcfa507d0eb3
Kelli Lilly <kelli.lilly05@gmail.com>
2022-12-08 12:37:23 -0600

    Use string with old/new ways to concatenate

--- app/index.js
+++ app/index.js
@@ -6,17 +6,13 @@ const lastName = "Cowin";
 const product1 = "T-Shirt";
 const product2 = "Socks";
 
-const price1 = 10;
-const price2 = 5;
-const salesTax = 0.07;
-
-// TODO: calculate total price of products
-const totalPrice = calcPriceOf2Items(p1, p2);
-
-// Don't reuse variable !!! Function parameters should NOT be the same as scope
-function calcTotalPriceWithTax(total, taxRate) {
-  return total + total * salesTax;
+function getFullName(fName, lName) {
+  return fName + " " + lName;
 }
 
-const totalPriceWithTax = calcTotalPriceWithTax(totalPrice, salesTax);
-console.log(totalPriceWithTax);
+const welcomeMsg = "Welcome to the store," + getFullName(firstName, lastName);
+
+// Template literals with interpolation - USE backticks and add $ to implement JS
+const welcomeMsg2 = `Welcome to the store, ${getFullName(firstName, lastName)}`;
+
+console.log(welcomeMsg);
