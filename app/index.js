7eb930ecd0a40e95bb9e649357975d2a92255f3e
Kelli Lilly <kelli.lilly05@gmail.com>
2022-12-08 11:36:40 -0600

    Play with number functions in node

--- app/index.js
+++ app/index.js
@@ -5,3 +5,17 @@ function add2Nums(num1, num2) {
 const sum = add2Nums(1, 2);
 
 console.log(sum);
+
+function sub(num1, num2) {
+  return num1 - num2;
+}
+
+const equal = sub(99, 98);
+console.log(equal);
+
+function multiplyWhatever(num1, num2) {
+  return num1 * num2;
+}
+
+const multiply = multiplyWhatever(4, 8);
+console.log(multiply);
