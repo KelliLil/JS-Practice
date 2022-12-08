328bc799ec4eab348842068ea0caf7a1a23dc09e
Kelli Lilly <kelli.lilly05@gmail.com>
2022-12-08 12:50:20 -0600

    Add discount input/output

--- app/index.js
+++ app/index.js
@@ -6,6 +6,9 @@ const lastName = "Cowin";
 const product1 = "T-Shirt";
 const product2 = "Socks";
 
+const cartTotal = 100;
+const discount = 0.1;
+
 function getFullName(fName, lName) {
   return fName + " " + lName;
 }
@@ -16,3 +19,36 @@ const welcomeMsg = "Welcome to the store," + getFullName(firstName, lastName);
 const welcomeMsg2 = `Welcome to the store, ${getFullName(firstName, lastName)}`;
 
 console.log(welcomeMsg);
+
+// TODO: write an 'if' statement such that is total is over $100, get 10% discount. Otherwise no discount. Log out the correct output. Use template literals to interpolate discount amount and the tolal
+
+if (cartTotal > 100) {
+  console.log(`You get a ${discount*100}%`discount!);
+  // Create sample data for a e-commerce store
+const username = "joec1982";
+const firstName = "Joe";
+const lastName = "Cowin";
+
+const product1 = "T-Shirt";
+const product2 = "Socks";
+
+const cartTotal = 101;
+const discount = 0.1;
+
+function getFullName(fName, lName) {
+  return fName + " " + lName;
+}
+
+// Template literals with interpolation
+const welcomeMsg = `Welcome to the store, ${getFullName(firstName, lastName)}`;
+
+console.log(welcomeMsg);
+
+// TODO: Write an 'if' statement such that if the total is over $100, the user gets a 10% discount. Otherwise, there is no discount. Either way log out the correct output. Use template literals to interpolate the discount amount and the total.
+
+if (cartTotal > 100) {
+  console.log(`You get a ${discount * 100}% discount!`);
+  console.log(`Your total is now $${cartTotal * (1 - discount)}`);
+} else {
+  console.log(`Your total is $${cartTotal}`);
+}
