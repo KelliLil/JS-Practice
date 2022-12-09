fe66a85b96deb5a5fdd60dddab83dd2695ba5923
Kelli Lilly <kelli.lilly05@gmail.com>
2022-12-09 10:45:39 -0600

    Create object collection

--- app/index.js
+++ app/index.js
@@ -29,8 +29,11 @@ const username = "joec1982";
 const firstName = "Joe";
 const lastName = "Cowin";
 
-const product1 = "T-Shirt";
-const product2 = "Socks";
+const product = {
+  name: "T-Shirt",
+  price: 5.99,
+  desc: "Soft Jersey Knit",
+}
 
 const cartTotal = 101;
 const discount = 0.1;
@@ -44,11 +47,9 @@ const welcomeMsg = `Welcome to the store, ${getFullName(firstName, lastName)}`;
 
 console.log(welcomeMsg);
 
-// TODO: Write an 'if' statement such that if the total is over $100, the user gets a 10% discount. Otherwise, there is no discount. Either way log out the correct output. Use template literals to interpolate the discount amount and the total.
-
 if (cartTotal > 100) {
   console.log(`You get a ${discount * 100}% discount!`);
-  console.log(`Your total is now $${cartTotal * (1 - discount)}`);
+  console.log(`Your total is now $${cartTotal - cartTotal * discount}`);
 } else {
   console.log(`Your total is $${cartTotal}`);
 }
