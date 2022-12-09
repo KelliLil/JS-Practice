ae2777aaab1f0e87fe6820000c77337fe2e70a93
Kelli Lilly <kelli.lilly05@gmail.com>
2022-12-09 11:42:14 -0600

    Add on to array

--- app/index.js
+++ app/index.js
@@ -29,11 +29,29 @@ const username = "joec1982";
 const firstName = "Joe";
 const lastName = "Cowin";
 
-const product = {
-  name: "T-Shirt",
-  price: 5.99,
-  desc: "Soft Jersey Knit",
-}
+const products = [
+  {
+    name: "Socks",
+    price: 5.99,
+    description: "A pair of warm, fuzzy socks",
+    imgUrl:
+      "https://images.unsplash.com/photo-1556740732-3f8f0a6c7b0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+  },
+  {
+    name: "Pants",
+    price: 10.99,
+    description: "A nice pair of pants",
+    imgUrl:
+      "https://images.unsplash.com/photo-1556740732-3f8f0a6c7b0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+  },
+  {
+    name: "Shoes",
+    price: 25.99,
+    description: "A nice pair of shoes",
+    imgUrl:
+      "https://images.unsplash.com/photo-1556740732-3f8f0a6c7b0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+  },
+];
 
 // Template literal - looks like creating a catalogue using JS
 function createProductTile(prod) {
@@ -48,6 +66,9 @@ function createProductTile(prod) {
   </figure>
   `;
 }
+const firstProduct = products[0];
+console.log(createProductTile(firstProduct));
+
 const productTile = createProductTile(product);
 console.log(productTile);
 
@@ -69,3 +90,18 @@ if (cartTotal > 100) {
 } else {
   console.log(`Your total is $${cartTotal}`);
 }
+
+// Arrays are collections that are indexed by implicitly generated numbers
+// The first item in the array is at index 0, the second at index 1, and so on
+// Arrays are zero-indexed, meaning the first item is at index 0
+const num = ["Socks","Shoes","Pants","Shirt","Hat"];
+
+const fistItem = num[0]
+const secondItem = num[1]
+const thirdItem = num[2]
+
+console.log(firstItem)
+console.log(secondItem)
+console.log(thirdItem)
+
+const catalog = []
