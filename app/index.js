71c9fcfcbe406ed79b51ea0c481afbbbd71f2305
Kelli Lilly <kelli.lilly05@gmail.com>
2022-12-14 11:11:05 -0600

    Use async to fetch

--- app/index.js
+++ app/index.js
@@ -1,30 +1,25 @@
-import people from "./people.js";
+// This will leverage the OS's thread pool to perform work
+// This is the most common way to perform asynchronous code
+// step into the waiting room and ready for appt, wait in second room to listen for when your turn
+const response = await fetch("https://jsonplaceholder.typicode.com/users");
 
+// Asynchronously read raw response data as JSON
+const people = await response.json();
 
+const createBioCard = (person) => `
+    <section>
+      <h2>${person.name}</h2>
+      <ul>
+        <li><a href="mailto:${person.email}">${person.email}</a></li>
+        <li><a href="tel:${person.phone}">${person.phone}</a></li>
+        <li><a href="https://www.google.com/maps/place/${person.address.geo.lat},${person.address.geo.lng}">${person.address.street}, ${person.address.city}</a></li>
+      </ul>
+      <footer>
+        <em>${person.company.catchPhrase}</em>
+      </footer>
+    </section>
+    `;
 
-console.log(people);
+const bioCardHTML = users.map(createBioCard).join("");
 
-const olderPeople = people.filter(
-  (theCurrentPersonThatIAmCheckingTheAgeFor4) =>
-    theCurrentPersonThatIAmCheckingTheAgeFor4.age > 27
-);
-
-const peopleThatStartWithJ = people.filter((person) =>
-  person.name.startsWith("J")
-);
-
-const peopleWithSalutation = people.map(
-  (person) => `${person.salutation} ${person.name}`
-);
-
-console.log(peopleWithSalutation);
-
-function createBioCard(person) {
-  return `
-    <p>${person.salutation} ${person.name} is ${person.age} years old.</p>
-  `;
-}
-
-const bioCards = people.map(createBioCard).join("");
-
-console.log(bioCards);
+console.log(bioCardHTML);
